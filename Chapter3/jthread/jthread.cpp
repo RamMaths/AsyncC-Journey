1af6171fcfa043c9c557a4b7d7e06cab6eeca1fe
Ramses <ramses.hdz30@gmail.com>
2025-03-11 23:14:26 -0600

    yielding threads

---
+++ Chapter3/jthread/jthread.cpp
@@ -0,0 +1,42 @@
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <syncstream>
+#include <thread>
+
+#define sync_cout std::osyncstream(std::cout)
+using namespace std::chrono_literals;
+
+class JthreadWrapper {
+public:
+    JthreadWrapper(
+        const std::function<void(const std::string&)>& func,
+        const std::string& str)
+    : t(func, str), name(str) {
+        sync_cout << "Thread " << name
+            << " being created" << std::endl;
+    }
+
+    ~JthreadWrapper() {
+        sync_cout << "Thread " << name
+        << " being destroyed" << std::endl;
+    }
+private:
+    std::jthread t;
+    std::string name;
+};
+
+void func(const std::string& name) {
+    sync_cout << "Thread " << name << " starting...\n";
+    std::this_thread::sleep_for(1s);
+    sync_cout << "Thread " << name << " finishing...\n";
+}
+
+int main() {
+    JthreadWrapper t1(func, "t1");
+    JthreadWrapper t2(func, "t2");
+    JthreadWrapper t3(func, "t3");
+    std::this_thread::sleep_for(2s);
+    sync_cout << "Main thread exiting..." << std::endl;
+    return 0;
+}
